691c40250e6eeb1582ee3729e93d2276012660d2
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-08-11 00:15:47 +0900

    bootstrap に実行権限を付与

--- lambda-container/build.sh
+++ lambda-container/build.sh
@@ -3,6 +3,7 @@ parent=$(dirname ${0})
 cd "$parent/../"
 cargo lambda build --bin bootstrap --release
 cp -f "target/lambda/bootstrap/bootstrap" "$parent/bootstrap"
+chmod 755 bootstrap
 
 cd $parent
 docker build --tag dual-judge-lambda .
